2527c8e6424cfecedc2cfcfeb440c5a649bcd2e6
hankotanks <hank@modernatomic.com>
2023-09-21 12:39:52 -0600

    Configured template

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'phonemenomicon-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './phonemenomicon.js',
+  './phonemenomicon_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
